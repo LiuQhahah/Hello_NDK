9f0b5968c405057af476d02207c99707de9c52d5
liuqiang <liuq_1995@163.com>
2017-01-04 11:25:27 +0800

    1，首次提交

---
+++ app/src/main/java/edu/njupt/liuq/hello_ndk/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.njupt.liuq.hello_ndk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
